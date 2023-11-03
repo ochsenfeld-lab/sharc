ec0a9fbd833227658413df196ea086863ed6581c
ri85moz <martin.peschel@lmu.de>
2023-11-03 14:41:25 +0100

    some more rdv changes

--- setup_rdv/setup_orca.sh
+++ setup_rdv/setup_orca.sh
@@ -6,6 +6,6 @@ export PATH=${PATH}:/opt/adm/orca/5.0.4/dynamic_openmpi411
 export PATH=${PATH}:/opt/adm/compile/openmpi/4.1.1/bin
 export ORCA=/opt/adm/orca/5.0.4/dynamic_openmpi411
 
-export SCRADIR=/home.io/map/sharc/sharc-3.0.1/TEST/ORCA/WORK/
+export SCRADIR=$SCRATCH
 source /opt/adm/theodore/2.4.0/setpaths.bash
 export THEODORE=$THEODIR
