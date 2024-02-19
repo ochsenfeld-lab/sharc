551f8f2a9f396d5df18b3a7da000e85147f79535
ri85moz <martin.peschel@lmu.de>
2024-02-19 15:21:39 +0100

    fixed offset of date when reading output.log

--- bin/diagnostics.py
+++ bin/diagnostics.py
@@ -653,11 +653,11 @@ def check_termination(path, trajectories, INFOS, f):
         countmax = min(10, trajectories[path]['laststep'])
         for line in range(len(f)):
             if 'ntering timestep' in f[line].lower():
-                check_old = f[line + 2].split()
+                check_old = f[line + 3].split()
                 if check_old[0] == "Start":
-                    timesteps.append(f[line + 2].strip()[12:])
+                    timesteps.append(f[line + 3].strip()[12:])
                 else:
-                    timesteps.append(f[line + 2].strip())
+                    timesteps.append(f[line + 3].strip())
                 count += 1
             if count == countmax:
                 break
@@ -668,12 +668,12 @@ def check_termination(path, trajectories, INFOS, f):
             total += tend - tstart
         for line in range(len(f)):
             if 'ntering timestep' in f[-line].lower():
-                check_old = f[-line + 2].split()
+                check_old = f[-line + 3].split()
                 if check_old[0] == "Start":
-                    tstart = datetime.datetime.strptime(f[-line + 2].strip()[12:], '%a %b %d %H:%M:%S %Y')
+                    tstart = datetime.datetime.strptime(f[-line + 3].strip()[12:], '%a %b %d %H:%M:%S %Y')
                     break
                 else:
-                    tstart = datetime.datetime.strptime(f[-line + 2].strip(), '%a %b %d %H:%M:%S %Y')
+                    tstart = datetime.datetime.strptime(f[-line + 3].strip(), '%a %b %d %H:%M:%S %Y')
                     break
         tend = datetime.datetime.now()
         tdiff = tend - tstart
